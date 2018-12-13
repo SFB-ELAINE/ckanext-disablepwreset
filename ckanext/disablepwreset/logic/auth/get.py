51d004013ee0b3a548157c29099263d8beba6885
Max Schröder <max.schroeder@uni-rostock.de>
2018-12-13 14:49:24 +0100

    Fixed import reference

--- ckanext/disablepwreset/logic/auth/get.py
+++ ckanext/disablepwreset/logic/auth/get.py
@@ -5,10 +5,10 @@ import ckan.authz as authz
 
 from ckan.common import _
 
-from ckanext.disablepwreset.plugin import config
+import ckanext.disablepwreset.plugin as p
 
 def _permit_reset():
-    permit_reset = config['ckanext.disablepwreset.permit_reset']
+    permit_reset = p.config['ckanext.disablepwreset.permit_reset']
 
     if not permit_reset:
         return {'success': False, 'msg': _('Not authorized to '
