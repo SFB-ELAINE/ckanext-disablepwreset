a4202049eef3bec27eb5bf58c2fac5ecffb3c05b
Max Schröder <max.schroeder@uni-rostock.de>
2018-12-13 16:42:36 +0100

    add missing annotation allowing anonymous access; moved to chained auth

--- ckanext/disablepwreset/logic/auth/get.py
+++ ckanext/disablepwreset/logic/auth/get.py
@@ -1,13 +1,24 @@
 # encoding: utf-8
 
-import ckan.logic as logic
-import ckan.authz as authz
-
+from ckan.plugins import toolkit
 from ckan.common import _
 
 from ckanext.disablepwreset.plugin import config
 
-def _permit_reset():
+
+@toolkit.chained_auth_function
+@toolkit.auth_allow_anonymous_access
+def user_reset(next_auth, context, data_dict=None):
+    return _permit_reset(next_auth, context, data_dict)
+
+
+@toolkit.chained_auth_function
+@toolkit.auth_allow_anonymous_access
+def request_reset(next_auth, context, data_dict=None):
+    return _permit_reset(next_auth, context, data_dict)
+
+
+def _permit_reset(next_auth, context, data_dict):
     permit_reset = config[u'ckanext.disablepwreset.permit_reset']
 
     if not permit_reset:
@@ -15,10 +26,4 @@ def _permit_reset():
             u'success': False,
             u'msg': _(u'Not authorized to reset password')
         }
-    return {u'success': True}
-
-def user_reset(context, data_dict):
-    return _permit_reset()
-
-def request_reset(context, data_dict):
-    return _permit_reset()
+    return next_auth(context, data_dict)
