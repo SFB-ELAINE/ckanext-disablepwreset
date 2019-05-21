c0a2d57e915c0c808e7228ed1eeb2d56dd6fd8cb
Max Schröder <max.schroeder@uni-rostock.de>
2019-05-21 20:04:53 +0200

    Removed chained auth due to compatibility

--- ckanext/disablepwreset/logic/auth/get.py
+++ ckanext/disablepwreset/logic/auth/get.py
@@ -6,19 +6,17 @@ from ckan.common import _
 from ckanext.disablepwreset.plugin import config
 
 
-@toolkit.chained_auth_function
 @toolkit.auth_allow_anonymous_access
-def user_reset(next_auth, context, data_dict=None):
-    return _permit_reset(next_auth, context, data_dict)
+def user_reset(context, data_dict=None):
+    return _permit_reset(context, data_dict)
 
 
-@toolkit.chained_auth_function
 @toolkit.auth_allow_anonymous_access
-def request_reset(next_auth, context, data_dict=None):
-    return _permit_reset(next_auth, context, data_dict)
+def request_reset(context, data_dict=None):
+    return _permit_reset(context, data_dict)
 
 
-def _permit_reset(next_auth, context, data_dict):
+def _permit_reset(context, data_dict):
     permit_reset = config[u'ckanext.disablepwreset.permit_reset']
 
     if not permit_reset:
@@ -26,4 +24,4 @@ def _permit_reset(next_auth, context, data_dict):
             u'success': False,
             u'msg': _(u'Not authorized to reset password')
         }
-    return next_auth(context, data_dict)
+    return {u'success':True}
